3d570024bd4cba9a0d7d9f33f52436426d93798d
Michal-Drobniak <michal.drobniak@schibsted.pl>
2016-09-13 14:28:42 +0200

    Add support for dynamic content of variants

--- test/browser/core.test.jsx
+++ test/browser/core.test.jsx
@@ -103,5 +103,67 @@ describe("Core Experiment", function() {
     assert.notEqual(elementB, null);
     ReactDOM.unmountComponentAtNode(container);
   }));
+  it("should update children components when their props change", co.wrap(function *(){
+    let experimentName = UUID.v4();
+
+    let SubComponent = React.createClass({
+      render(){
+        return (
+            <div id="variant-a">
+              <span id="variant-a-text">{this.props.text}</span>
+            </div>
+        )
+      }
+    });
+
+    let variantAText = "Initial text";
+
+    let renderHelper = {
+      init(callback) {
+        this.callback = callback;
+      },
+      reRender() {
+        this.callback();
+      }
+    };
+
+    let App = React.createClass({
+      componentDidMount(){
+        renderHelper.init(this.callback);
+      },
+      callback() {
+        this.forceUpdate();
+      },
+      render(){
+        return (
+          <Experiment name={experimentName} value="A" isContentDynamic>
+            <Variant name="A">
+              <SubComponent text={variantAText}/>
+            </Variant>
+            <Variant name="B"><div id="variant-b" /></Variant>
+          </Experiment>
+        )
+      }
+    });
+    yield new Promise(function(resolve, reject){
+      ReactDOM.render(<App />, container, resolve);
+    });
+    let elementA = document.getElementById('variant-a');
+    let elementB = document.getElementById('variant-b');
+    assert.notEqual(elementA, null);
+    assert.equal(elementB, null);
+
+    let elementAText = document.getElementById('variant-a-text');
+    console.log(elementAText.textContent);
+    assert.equal(elementAText.textContent, variantAText);
+
+    variantAText = "Initial text 2";
+    renderHelper.reRender();
+    elementAText = document.getElementById('variant-a-text');
+
+    assert.equal(elementAText.textContent, variantAText);
+
+    ReactDOM.unmountComponentAtNode(container);
+  }));
 });
 
