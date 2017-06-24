d2b119d55338700a81d3e722180e665e2be9180c
alexeyMohnatkin <dizzymohnatkin@gmail.com>
2017-06-24 13:40:11 +0300

    update tests

--- test/isomorphic/experiment.jsx
+++ test/isomorphic/experiment.jsx
@@ -4,50 +4,71 @@ import Experiment from "../../src/Experiment.jsx";
 import Variant from "../../src/Variant.jsx";
 import emitter from "../../src/emitter.jsx";
 import assert from "assert";
-import co from "co";
+import { expect } from "chai";
 import UUID from "node-uuid";
 
-describe("Experiment", function() {
-  this.timeout(10000);
-  it("should render to a string.", co.wrap(function *(){
-    let experimentName = UUID.v4();
-    let App = React.createClass({
-      render: function(){
-        return <Experiment name={experimentName}>
-          <Variant name="A"><div id="variant-a" /></Variant>
-          <Variant name="B"><div id="variant-b" /></Variant>
-        </Experiment>;
-      }
-    });
-    let result = ReactDOMServer.renderToString(<App />);
-  }));
-  it("should choose the same variant when a user identifier is defined.", co.wrap(function *(){
-    let userIdentifier = UUID.v4();
-    let experimentName = UUID.v4();
-    let variantNames = [];
+const renderApp = (experimentName, variantNames, userIdentifier) => {
+  return () =>
+  	<Experiment name={experimentName} userIdentifier={userIdentifier}>
+      {variantNames.map(name => {
+        return <Variant key={name} name={name}><div id={'variant-' + name}></div></Variant>
+      })}
+    </Experiment>
+}
+
+describe("Experiment", () => {
+
+
+  it("should render to a string.", () => {
+    const experimentName = UUID.v4();
+    const App = () =>
+    	<Experiment name={experimentName}>
+        <Variant name="A"><div id="variant-a" /></Variant>
+        <Variant name="B"><div id="variant-b" /></Variant>
+      </Experiment>
+
+    const result = ReactDOMServer.renderToString(<App />);
+    expect(result).to.be.a('string');
+  });
+  it("should choose the same variant when a user identifier is defined.", () => {
+    const userIdentifier = UUID.v4();
+    const experimentName = UUID.v4();
+    const variantNames = [];
     for(let i = 0; i < 100; i++) {
       variantNames.push(UUID.v4());
     }
-    let App = React.createClass({
-      render: function(){
-        return <Experiment name={experimentName} userIdentifier={userIdentifier}>
-          {variantNames.map(name => {
-            return <Variant key={name} name={name}><div id={'variant-' + name}></div></Variant>
-          })}
-        </Experiment>;
-      }
-    });
+
+    const App = renderApp(experimentName, variantNames, userIdentifier);
+
     let chosenVariant;
-    emitter.once("play", function(experimentName, variantName){
+    emitter.once("play", (experimentName, variantName) => {
       chosenVariant = variantName;
     });
-    let result = ReactDOMServer.renderToString(<App />);
+
+    const result = ReactDOMServer.renderToString(<App />);
     assert(chosenVariant);
+
     assert.notEqual(result.indexOf(chosenVariant), -1);
     for(let i = 0; i < 100; i++) {
       emitter._reset();
-      result = ReactDOMServer.renderToString(<App />);
-      assert.notEqual(result.indexOf(chosenVariant), -1);
+      const res = ReactDOMServer.renderToString(<App />);
+      assert.notEqual(res.indexOf(chosenVariant), -1);
     }
-  }));
+  });
+  it("should render different variants with different user identifiers.", () => {
+  	const userIdentifier = UUID.v4();
+    const user2Identifier = UUID.v4();
+    const experimentName = UUID.v4();
+    const variantNames = [];
+    for(let i = 0; i < 100; i++) {
+      variantNames.push(UUID.v4());
+    }
+  	const FirstRender = renderApp(experimentName, variantNames, userIdentifier);
+    const SecondRender = renderApp(experimentName, variantNames, user2Identifier);
+
+    const result1 = ReactDOMServer.renderToString(<FirstRender />);
+    const result2 = ReactDOMServer.renderToString(<SecondRender />);
+
+    assert.notEqual(result1, result2);
+  });
 });
