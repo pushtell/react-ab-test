b9b66dbf436c72c935dc8fadf608006712423ad4
alexeyMohnatkin <dizzymohnatkin@gmail.com>
2017-06-24 13:48:19 +0300

    add comments

--- src/Experiment.jsx
+++ src/Experiment.jsx
@@ -48,28 +48,53 @@ export default class Experiment extends Component {
     emitter.emitWin(this.props.name);
   };
 
-  getRandomValue = () => {
+  getSelectedVariant = () => {
+    /*
+
+    Choosing a weighted variant:
+      For C, A, B with weights 2, 4, 8
+
+      variants = A, B, C
+      weights = 4, 8, 2
+      weightSum = 14
+      weightedIndex = 9
+
+      AAAABBBBBBBBCC
+      ========^
+      Select B
+
+    */
+
+    // Sorted array of the variant names, example: ["A", "B", "C"]
     const variants = emitter.getSortedVariants(this.props.name);
+    // Array of the variant weights, also sorted by variant name. For example, if
+    // variant C had weight 2, variant A had weight 4, and variant B had weight 8
+    // return [4, 8, 2] to correspond with ["A", "B", "C"]
     const weights = emitter.getSortedVariantWeights(this.props.name);
+    // Sum the weights
     const weightSum = weights.reduce((a, b) => {
       return a + b;
     }, 0);
+    // A random number between 0 and weightSum
     let weightedIndex = typeof this.props.userIdentifier === 'string' ? Math.abs(crc32(this.props.userIdentifier) % weightSum) : Math.floor(Math.random() * weightSum);
-    let randomValue = variants[variants.length - 1];
+    // Iterate through the sorted weights, and deduct each from the weightedIndex.
+    // If weightedIndex drops < 0, select the variant. If weightedIndex does not
+    // drop < 0, default to the last variant in the array that is initially assigned.
+    let selectedVariant = variants[variants.length - 1];
     for (let index = 0; index < weights.length; index++) {
       weightedIndex -= weights[index];
       if (weightedIndex < 0) {
-        randomValue = variants[index];
+        selectedVariant = variants[index];
         break;
       }
     }
-    emitter.setActiveVariant(this.props.name, randomValue);
-    return randomValue;
+    emitter.setActiveVariant(this.props.name, selectedVariant);
+    return selectedVariant;
   }
 
   getLocalStorageValue = () => {
     if(typeof this.props.userIdentifier === "string") {
-      return this.getRandomValue();
+      return this.getSelectedVariant();
     }
     const activeValue = emitter.getActiveVariant(this.props.name);
     if(typeof activeValue === "string") {
@@ -84,7 +109,7 @@ export default class Experiment extends Component {
       emitter.setActiveVariant(this.props.name, this.props.defaultVariantName);
       return this.props.defaultVariantName;
     }
-    return this.getRandomValue();
+    return this.getSelectedVariant();
   }
 
   render() {
